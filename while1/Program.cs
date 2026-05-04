28c4ca7b12711c1cb528d4d2bd9b4274a9ef574f
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-04 10:30:00 -0600

    ejercicio1_while

---
+++ while1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Leer un número entero y mostrar todos sus antecesores elevados al cubo
+using Math = System.Math;
+Console.Clear();
+int numero;
+while (true)
+{
+    Console.Write("Escribe un número: ");
+    if (int.TryParse(Console.ReadLine(), out numero))
+    {
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine("Error. Ingrese un número entero");
+        Console.ResetColor();
+    }
+}
+Console.WriteLine("Los antecesores elevados al cubo son:");
+int contador = 1;
+while (contador<numero)
+{
+    double cubo = Math.Pow(contador, 3);
+    Console.WriteLine($"{contador}^3 = {cubo}");
+    contador++;
+}

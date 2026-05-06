d4e615beb51f3f3765f7c1c7da36017cf85e0dd2
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-06 10:38:23 -0600

    feat: add proyecto arreglo1

---
+++ arreglo1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Almacenar 10 edades en un arreglo, luego calcular el promedio de edad, luego el máximo y el mínimo.
+//crear arreglo de edades
+int[] edades = new int[10];
+//Pedir edades
+for (int i = 0; i < edades.Length; i++)
+{
+    try
+    {
+        Console.WriteLine("Ingrese la edad " + (i + 1) + ":");
+        edades[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine("Por favor, ingrese un número válido.");
+        i--; // Para volver a pedir la misma posición
+        Console.ResetColor();
+    }
+}
+
+//Calcular promedio maximo y minimo
+int suma = 0, maximo = edades[0], minimo = edades[0];
+foreach (int edad in edades)
+{
+    suma += edad;
+    if (edad > maximo) maximo = edad;
+    if (edad < minimo) minimo = edad;
+}
+double promedio = (double)suma / edades.Length;
+//Mostrar resultados
+Console.WriteLine("El promedio de edad es: " + promedio);
+Console.WriteLine("La edad máxima es: " + maximo);
+Console.WriteLine("La edad mínima es: " + minimo);
---
+++ arreglo2/Program.cs
@@ -0,0 +1,56 @@
+﻿//Crea un arreglo de notas de 15 estudiantes, luego calcula el promedio de las notas y los 3 primeros lugares. No se puede ingresar notas menores a 0 ni mayores a 100.
+int [] notas = new int[15];
+int suma = 0, conterror = 0;
+double promedio = 0;
+for (int i = 0; i < notas.Length; i++)
+{
+    if (conterror >= 3)
+    {
+        Console.ForegroundColor=ConsoleColor.DarkRed;
+        Console.WriteLine("Has cometido demasiados errores, es normal, te recomiendo descansar.");
+        Console.ResetColor();
+    }
+    try
+    {
+        Console.WriteLine("Ingrese la nota del estudiante " + (i + 1) + ":");
+        int nota = int.Parse(Console.ReadLine()!);
+        if (nota < 0 || nota > 100)
+        {
+            throw new ArgumentOutOfRangeException("La nota debe estar entre 0 y 100.");
+        }
+        notas [i] = nota;
+        
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine("Por favor, ingrese un número válido.");
+        i--; // Para volver a pedir la misma posición
+        Console.ResetColor();
+        conterror++;
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine(ex.Message);
+        conterror++;
+        i--; // Para volver a pedir la misma posición
+        Console.ResetColor();
+    }
+}
+
+foreach(int nota in notas)
+{
+    suma += nota;
+}
+Console.WriteLine($"Suma: {suma}");
+Console.WriteLine($"Cantidad de notas: {notas.Length}");
+promedio = (double)suma / notas.Length;
+Console.WriteLine($"Promedio: {promedio:F2}");
+
+Array.Sort(notas);
+Array.Reverse(notas);
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Lugar {i + 1}: {notas[i]}");
+}

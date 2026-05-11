31ef25a1efd8bcec52f4bf4a487d70972cf76c3b
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-11 10:45:38 -0600

    Actualización

---
+++ s9e1_temperatura/Program.cs
@@ -0,0 +1,57 @@
+﻿//Ejercicio 1
+//Temperaturas de la Semana
+//Una estación meteorológica necesita registrar las temperaturas máximas de 7 días.
+//El programa debe:
+//Solicitar al usuario las 7 temperaturas.
+//Guardar los datos en un arreglo unidimensional.
+//Mostrar todas las temperaturas registradas.
+//Calcular el promedio semanal.
+//Indicar la temperatura más alta y la más baja.
+Console.Clear();
+double [] temp = new double [7];
+double sumatemp = 0;
+Console.WriteLine("ESTACIÓN METEOROLÓGICA");
+for (int i=0 ; i< temp.Length ; i++)
+{
+    try
+    {
+        Console.Write($"Día #{i+1}: ");
+        temp[i]=double.Parse(Console.ReadLine()!);
+        sumatemp += temp[i];
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Por favor ingrese un número válido");
+        i--; 
+        Console.ResetColor();
+    }
+}
+double promedio = sumatemp/temp.Length;
+
+double mayor = temp[0];
+double menor = temp[0];
+
+for (int i = 0; i < temp.Length; i++)
+{
+    if (temp[i] > mayor)
+    {
+        mayor = temp[i];
+    }
+
+    if (temp[i] < menor)
+    {
+        menor = temp[i];
+    }
+}
+Console.Clear();
+Console.WriteLine("TEMPERATURAS REGISTRADAS");
+
+for (int i = 0; i < temp.Length; i++)
+{
+    Console.WriteLine($"Día #{i + 1}: {temp[i]}°C");
+}
+
+Console.WriteLine($"\nPromedio semanal: {Math.Round(promedio,2)}");
+Console.WriteLine($"Temperatura más alta: {mayor}");
+Console.WriteLine($"Temperatura más baja: {menor}");
---
+++ s9e2_tienda/Program.cs
@@ -0,0 +1,64 @@
+﻿//Ejercicio 2
+//Ventas de una Tienda
+//Una tienda desea registrar las ventas diarias de una semana.
+//El programa debe:
+//Solicitar al usuario las ventas de cada día.
+//Guardar las ventas en un arreglo.
+//Calcular el total vendido.
+//Mostrar cuántos días las ventas superaron 150.
+//Indicar la venta más baja registrada.
+Console.Clear();
+int num_ventas = -1;
+
+while (num_ventas < 0)
+{
+    try
+    {
+        Console.Write("Número de ventas diarias: ");
+        num_ventas = int.Parse(Console.ReadLine()!);
+
+        if (num_ventas < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error. Ingrese un número válido.");
+            Console.ResetColor();
+        }
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error. Ingrese un número entero válido.");
+        Console.ResetColor();
+
+        num_ventas = -1;
+    }
+}
+
+if (num_ventas == 0)
+{
+    Console.WriteLine("Sin ventas");
+}
+else
+{
+    double[] ventas = new double[num_ventas];
+    double sumaventas = 0;
+
+    for (int i = 0; i < ventas.Length; i++)
+    {
+        try
+        {
+            Console.Write($"Venta #{i + 1}: ");
+            ventas[i] = double.Parse(Console.ReadLine()!);
+            sumaventas += ventas[i];
+        }
+        catch (FormatException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Por favor ingrese un número válido");
+            i--;
+            Console.ResetColor();
+        }
+    }
+
+    Console.WriteLine($"Total vendido: {sumaventas}");
+}
